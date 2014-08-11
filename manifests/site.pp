a2534c9692445030dc829ff011ff3aeb643bc876
Becca Gaspard <beccagaspard@gmail.com>
2014-08-10 22:10:53 -0400

    testing...

--- manifests/site.pp
+++ manifests/site.pp
@@ -8,9 +8,9 @@ Exec {
   user        => $boxen_user,
 
   path => [
-    "${boxen::config::home}/rbenv/shims",
-    "${boxen::config::home}/rbenv/bin",
-    "${boxen::config::home}/rbenv/plugins/ruby-build/bin",
+#    "${boxen::config::home}/rbenv/shims",
+#    "${boxen::config::home}/rbenv/bin",
+#    "${boxen::config::home}/rbenv/plugins/ruby-build/bin",
     "${boxen::config::home}/homebrew/bin",
     '/usr/bin',
     '/bin',
@@ -53,27 +53,27 @@ Homebrew::Formula <| |> -> Package <| |>
 
 node default {
   # core modules, needed for most things
-  include dnsmasq
+  #include dnsmasq
   include git
-  include hub
-  include nginx
+  #include hub
+  #include nginx
 
   # fail if FDE is not enabled
-  if $::root_encrypted == 'no' {
-    fail('Please enable full disk encryption and try again')
-  }
+  #if $::root_encrypted == 'no' {
+  #  fail('Please enable full disk encryption and try again')
+  #}
 
   # node versions
-  include nodejs::v0_6
-  include nodejs::v0_8
-  include nodejs::v0_10
+  #include nodejs::v0_6
+  #include nodejs::v0_8
+  #include nodejs::v0_10
 
   # default ruby versions
-  ruby::version { '1.9.3': }
-  ruby::version { '2.0.0': }
-  ruby::version { '2.1.0': }
-  ruby::version { '2.1.1': }
-  ruby::version { '2.1.2': }
+  #ruby::version { '1.9.3': }
+  #ruby::version { '2.0.0': }
+  #ruby::version { '2.1.0': }
+  #ruby::version { '2.1.1': }
+  #ruby::version { '2.1.2': }
 
   # common, useful packages
   package {
